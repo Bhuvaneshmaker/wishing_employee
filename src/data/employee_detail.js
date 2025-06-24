08724411faed135b68508f0ae52cb90a929f1aaa
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-06-24 22:46:11 +0530

    Update employee_detail.js

--- src/data/employee_detail.js
+++ src/data/employee_detail.js
@@ -1,37 +1,479 @@
-const sampleEmployees = [
-{
-    id: 1,
-    name: "Hemnath Chandrasekaran",
-    birthday: "1994-09-02", // 2-Sep-1994
-    joinDate: "2017-05-17", //8-May-17
-
+  const sampleEmployees = [
+  {
+    "id": 1,
+    "name": "Hemnath Chandrasekaran",
+    "birthday": "Friday, September 02, 1994",
+    "joinDate": "Monday, May 08, 2017",
+    "": ""
   },
   {
-    id: 2,
-    name: "K ShyamalaDevi",
-    birthday: "1980-12-07", // 7-Dec-1980
-    joinDate: "2019-10-19", //10-Oct-19
-    
+    "id": 2,
+    "name": "K ShyamalaDevi",
+    "birthday": "Sunday, December 07, 1980",
+    "joinDate": "Thursday, October 10, 2019",
+    "": ""
   },
   {
-    id: 3,
-    name: "Muthamma Pandu",
-    birthday: "1970-02-06", // 6-Feb-1970
-    joinDate: "2022-01-15", //1-Feb-20
-
+    "id": 3,
+    "name": "Muthamma Pandu",
+    "birthday": "Friday, February 06, 1970",
+    "joinDate": "Saturday, February 01, 2020",
+    "": ""
   },
   {
-    id: 4,
-    name: "Venkatesh Arumugam",
-    birthday: "1999-12-17", // 17-Dec-1999
-    joinDate: "2020-11-05", //5-Nov-20
-
+    "id": 4,
+    "name": "Venkatesh Arumugam",
+    "birthday": "Friday, December 17, 1999",
+    "joinDate": "Thursday, November 05, 2020",
+    "": ""
   },
   {
-    id: 5,
-    name: "Lokesh Venkatesan",
-    birthday: "1996-05-10", // 10-May-1996
-    joinDate: "2020-12-01", // 1-Dec-20
-
+    "id": 5,
+    "name": "Lokesh Venkatesan",
+    "birthday": "Friday, May 10, 1996",
+    "joinDate": "Tuesday, December 01, 2020",
+    "": ""
   },
-  ];
+  {
+    "id": 6,
+    "name": "Rathinavel Palaniyandi",
+    "birthday": "Friday, July 07, 1995",
+    "joinDate": "Tuesday, December 01, 2020",
+    "": ""
+  },
+  {
+    "id": 7,
+    "name": "S.Karthik",
+    "birthday": "Wednesday, April 22, 1998",
+    "joinDate": "Friday, April 02, 2021",
+    "": ""
+  },
+  {
+    "id": 8,
+    "name": "S. Shunmugaraj",
+    "birthday": "Sunday, February 16, 1992",
+    "joinDate": "Monday, August 23, 2021",
+    "": ""
+  },
+  {
+    "id": 9,
+    "name": "Balamurugan",
+    "birthday": "Thursday, April 04, 2002",
+    "joinDate": "Monday, September 20, 2021",
+    "": ""
+  },
+  {
+    "id": 10,
+    "name": "P. Yuvarajasekar",
+    "birthday": "Tuesday, February 01, 2000",
+    "joinDate": "Monday, November 08, 2021",
+    "": ""
+  },
+  {
+    "id": 11,
+    "name": "P. Sudhakar",
+    "birthday": "Friday, October 28, 1988",
+    "joinDate": "Wednesday, November 10, 2021",
+    "": ""
+  },
+  {
+    "id": 12,
+    "name": "Ramkumar",
+    "birthday": "Friday, February 24, 1995",
+    "joinDate": "Thursday, November 11, 2021",
+    "": ""
+  },
+  {
+    "id": 13,
+    "name": "Sriram S",
+    "birthday": "Thursday, April 23, 1998",
+    "joinDate": "Thursday, November 11, 2021",
+    "": ""
+  },
+  {
+    "id": 14,
+    "name": "Pradeesh Ravi",
+    "birthday": "Saturday, May 01, 2004",
+    "joinDate": "Monday, December 20, 2021",
+    "": ""
+  },
+  {
+    "id": 15,
+    "name": "Hariharan Vellingiri",
+    "birthday": "Monday, June 12, 1995",
+    "joinDate": "Wednesday, February 02, 2022",
+    "": ""
+  },
+  {
+    "id": 16,
+    "name": "Viswanathan M",
+    "birthday": "Saturday, July 10, 1999",
+    "joinDate": "Monday, February 14, 2022",
+    "": ""
+  },
+  {
+    "id": 17,
+    "name": "Praveen Kumar",
+    "birthday": "Friday, August 04, 1995",
+    "joinDate": "Tuesday, March 29, 2022",
+    "": ""
+  },
+  {
+    "id": 18,
+    "name": "Vinoth Kuppusamy",
+    "birthday": "Friday, June 03, 1994",
+    "joinDate": "Friday, May 06, 2022",
+    "": ""
+  },
+  {
+    "id": 19,
+    "name": "Sade Subbaraju",
+    "birthday": "Monday, August 16, 1993",
+    "joinDate": "Tuesday, August 16, 2022",
+    "": ""
+  },
+  {
+    "id": 20,
+    "name": "Gowthamraj Vengatachalam",
+    "birthday": "Friday, March 26, 1999",
+    "joinDate": "Tuesday, August 16, 2022",
+    "": ""
+  },
+  {
+    "id": 21,
+    "name": "Ayyasamy Palani",
+    "birthday": "Wednesday, December 30, 1987",
+    "joinDate": "",
+    "": ""
+  },
+  {
+    "id": 22,
+    "name": "Suguna Viswanathan",
+    "birthday": "Tuesday, July 07, 1987",
+    "joinDate": "Monday, January 02, 2012",
+    "": ""
+  },
+  {
+    "id": 23,
+    "name": "Vignesh Kumar Murugan",
+    "birthday": "Monday, May 06, 1996",
+    "joinDate": "Thursday, June 15, 2017",
+    "": ""
+  },
+  {
+    "id": 24,
+    "name": "Natarajan R",
+    "birthday": "Friday, March 29, 1985",
+    "joinDate": "Saturday, March 03, 2018",
+    "": ""
+  },
+  {
+    "id": 25,
+    "name": "Kaviyan Raja",
+    "birthday": "Wednesday, February 02, 2000",
+    "joinDate": "Wednesday, June 12, 2019",
+    "": ""
+  },
+  {
+    "id": 26,
+    "name": "R.Meena",
+    "birthday": "Monday, July 05, 1999",
+    "joinDate": "Saturday, November 09, 2019",
+    "": ""
+  },
+  {
+    "id": 27,
+    "name": "Indhumathi Ramaraj",
+    "birthday": "Tuesday, February 15, 1994",
+    "joinDate": "Wednesday, April 21, 2021",
+    "": ""
+  },
+  {
+    "id": 28,
+    "name": "Ramraj Kumarasamy",
+    "birthday": "Thursday, May 10, 1984",
+    "joinDate": "Monday, September 06, 2021",
+    "": ""
+  },
+  {
+    "id": 29,
+    "name": "Kannan P U",
+    "birthday": "Thursday, May 09, 1996",
+    "joinDate": "Thursday, September 01, 2022",
+    "": ""
+  },
+  {
+    "id": 30,
+    "name": "Rajesh Chinnaraju",
+    "birthday": "Sunday, September 01, 1991",
+    "joinDate": "Thursday, December 08, 2022",
+    "": ""
+  },
+  {
+    "id": 31,
+    "name": "Kishore Kumar Sasumana",
+    "birthday": "Wednesday, September 18, 1985",
+    "joinDate": "Monday, December 26, 2022",
+    "": ""
+  },
+  {
+    "id": 32,
+    "name": "Suriya M",
+    "birthday": "Wednesday, December 26, 2001",
+    "joinDate": "Wednesday, February 01, 2023",
+    "": ""
+  },
+  {
+    "id": 33,
+    "name": "Eugene Jowshwa S",
+    "birthday": "Thursday, May 31, 2001",
+    "joinDate": "Monday, March 06, 2023",
+    "": ""
+  },
+  {
+    "id": 34,
+    "name": "Saranya Nataraj",
+    "birthday": "Friday, July 30, 1993",
+    "joinDate": "Monday, March 07, 2022",
+    "": ""
+  },
+  {
+    "id": 35,
+    "name": "Madhumidha",
+    "birthday": "Saturday, November 10, 1990",
+    "joinDate": "Thursday, December 01, 2022",
+    "": ""
+  },
+  {
+    "id": 36,
+    "name": "Jishnu Arusamy",
+    "birthday": "Wednesday, February 14, 1996",
+    "joinDate": "Wednesday, May 10, 2023",
+    "": ""
+  },
+  {
+    "id": 37,
+    "name": "Karmegan",
+    "birthday": "Saturday, May 08, 1999",
+    "joinDate": "Wednesday, June 14, 2023",
+    "": ""
+  },
+  {
+    "id": 38,
+    "name": "Kishankumar Srinivasan",
+    "birthday": "Sunday, December 14, 1997",
+    "joinDate": "Wednesday, June 14, 2023",
+    "": ""
+  },
+  {
+    "id": 39,
+    "name": "Thangatamizhan R",
+    "birthday": "Wednesday, November 14, 2001",
+    "joinDate": "Monday, July 17, 2023",
+    "": ""
+  },
+  {
+    "id": 40,
+    "name": "Mohammed Muhaiyadeen",
+    "birthday": "Wednesday, January 06, 1993",
+    "joinDate": "Wednesday, September 13, 2023",
+    "": ""
+  },
+  {
+    "id": 41,
+    "name": "Mohanraj Gnanasekar",
+    "birthday": "Friday, June 25, 1993",
+    "joinDate": "Tuesday, September 19, 2023",
+    "": ""
+  },
+  {
+    "id": 42,
+    "name": "Rakesh Murugan",
+    "birthday": "Tuesday, September 09, 1997",
+    "joinDate": "Friday, October 27, 2023",
+    "": ""
+  },
+  {
+    "id": 43,
+    "name": "R.Thangaraj",
+    "birthday": "Tuesday, May 15, 1990",
+    "joinDate": "Monday, November 20, 2023",
+    "": ""
+  },
+  {
+    "id": 44,
+    "name": "Tendulkar Prasanna T",
+    "birthday": "Monday, September 13, 1993",
+    "joinDate": "Wednesday, November 29, 2023",
+    "": ""
+  },
+  {
+    "id": 45,
+    "name": "Kaviraj K",
+    "birthday": "Sunday, October 23, 1994",
+    "joinDate": "Wednesday, December 06, 2023",
+    "": ""
+  },
+  {
+    "id": 46,
+    "name": "Santhoshini R",
+    "birthday": "Monday, June 21, 1999",
+    "joinDate": "Wednesday, December 20, 2023",
+    "": ""
+  },
+  {
+    "id": 47,
+    "name": "Nagasoundarya P",
+    "birthday": "Thursday, February 25, 1999",
+    "joinDate": "Monday, January 29, 2024",
+    "": ""
+  },
+  {
+    "id": 48,
+    "name": "N Thamarai Kannan ",
+    "birthday": "Friday, June 10, 1994",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 49,
+    "name": "Venish Siddharth P",
+    "birthday": "Tuesday, July 09, 2002",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 50,
+    "name": "P Sivakumar ",
+    "birthday": "Monday, May 29, 2000",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 51,
+    "name": "Yuvarani M",
+    "birthday": "Friday, March 16, 2001",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 52,
+    "name": "S Mohamed Shajith",
+    "birthday": "Saturday, July 14, 2001",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 53,
+    "name": "Jagankumar Kandasamy",
+    "birthday": "Sunday, February 26, 1995",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 54,
+    "name": "Hariharan S",
+    "birthday": "Wednesday, July 07, 1999",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 55,
+    "name": "Ponmanikandan Mani",
+    "birthday": "Saturday, December 11, 1999",
+    "joinDate": "Monday, April 01, 2024",
+    "": ""
+  },
+  {
+    "id": 56,
+    "name": "M.Vignesh",
+    "birthday": "Monday, May 19, 2003",
+    "joinDate": "Wednesday, April 10, 2024",
+    "": ""
+  },
+  {
+    "id": 57,
+    "name": "Sabarinath Udhayakumar",
+    "birthday": "Saturday, October 21, 1995",
+    "joinDate": "Thursday, May 02, 2024",
+    "": ""
+  },
+  {
+    "id": 58,
+    "name": "R.Meena",
+    "birthday": "Monday, July 05, 1999",
+    "joinDate": "Saturday, June 01, 2024",
+    "": ""
+  },
+  {
+    "id": 59,
+    "name": "Chennakrishnan M ",
+    "birthday": "Thursday, June 19, 2003",
+    "joinDate": "Thursday, June 06, 2024",
+    "": ""
+  },
+  {
+    "id": 60,
+    "name": "Chidambar Rao Desai",
+    "birthday": "Thursday, July 08, 1976",
+    "joinDate": "Wednesday, July 17, 2024",
+    "": ""
+  },
+  {
+    "id": 61,
+    "name": "KEERTHI LAKSHMANA",
+    "birthday": "Monday, May 06, 2002",
+    "joinDate": "Wednesday, November 13, 2024",
+    "": ""
+  },
+  {
+    "id": 62,
+    "name": "S.Mukundan",
+    "birthday": "Saturday, September 15, 2001",
+    "joinDate": "Wednesday, November 20, 2024",
+    "": ""
+  },
+  {
+    "id": 63,
+    "name": "Kavin thirumalaisamy",
+    "birthday": "Monday, July 17, 1995",
+    "joinDate": "Monday, December 02, 2024",
+    "": ""
+  },
+  {
+    "id": 64,
+    "name": "Sarathbabu L",
+    "birthday": "Wednesday, July 16, 1997",
+    "joinDate": "Friday, December 13, 2024",
+    "": ""
+  },
+  {
+    "id": 65,
+    "name": "Krishnakanth K",
+    "birthday": "Wednesday, January 08, 1992",
+    "joinDate": "Wednesday, January 29, 2025",
+    "": ""
+  },
+  {
+    "id": 66,
+    "name": "Paul Jeba Singh",
+    "birthday": "Tuesday, November 14, 2000",
+    "joinDate": "Monday, February 17, 2025",
+    "": ""
+  },
+  {
+    "id": 67,
+    "name": "Pooja Sekar",
+    "birthday": "Tuesday, October 17, 2000",
+    "joinDate": "Monday, March 03, 2025",
+    "": ""
+  },
+  {
+    "id": "",
+    "name": "",
+    "birthday": "",
+    "joinDate": "",
+    "": ""
+  }
+]
+export default sampleEmployees;
