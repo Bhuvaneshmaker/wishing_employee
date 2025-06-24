b5489c7ff0c7faa3729c904aa216b1638bddf187
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-06-24 22:06:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,44 +14,7 @@ const EmployeeBirthdayApp = () => {
   const [joinEmployee , setJoinEmployee] = useState([])
 
   // Sample data - replace with your Excel data
-  const sampleEmployees = [
-{
-    id: 1,
-    name: "Hemnath Chandrasekaran",
-    birthday: "1994-09-02", // 2-Sep-1994
-    joinDate: "2017-05-17", //8-May-17
-
-  },
-  {
-    id: 2,
-    name: "K ShyamalaDevi",
-    birthday: "1980-12-07", // 7-Dec-1980
-    joinDate: "2019-10-19", //10-Oct-19
-    
-  },
-  {
-    id: 3,
-    name: "Muthamma Pandu",
-    birthday: "1970-02-06", // 6-Feb-1970
-    joinDate: "2022-01-15", //1-Feb-20
-
-  },
-  {
-    id: 4,
-    name: "Venkatesh Arumugam",
-    birthday: "1999-12-17", // 17-Dec-1999
-    joinDate: "2020-11-05", //5-Nov-20
-
-  },
-  {
-    id: 5,
-    name: "Lokesh Venkatesan",
-    birthday: "1996-05-10", // 10-May-1996
-    joinDate: "2020-12-01", // 1-Dec-20
-
-  },
-  ];
-
+  
   useEffect(() => {
     setEmployees(sampleEmployees);
     checkTodaysBirthdays(sampleEmployees);
