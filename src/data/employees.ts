835a9178223db53480819d4569375351dc92969b
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-06-24 10:29:34 +0530

    Update employees.ts

--- src/data/employees.ts
+++ src/data/employees.ts
@@ -1,7 +1,7 @@
 export interface Employee {
   id: number;
   name: string;
-  birthday: string; // MM-DD-YYYY format
+  birthday: string; // YYYY-MM-DD format
   joinDate: string; // YYYY-MM-DD format
 
 }
@@ -11,84 +11,84 @@ export const employees: Employee[] = [
   {
     id: 1,
     name: "Hemnath Chandrasekaran",
-    birthday: "09-02-1994", // 2-Sep-1994
+    birthday: "1994-09-02", // 2-Sep-1994
     joinDate: "2017-05-17", //8-May-17
 
   },
   {
     id: 2,
     name: "K ShyamalaDevi",
-    birthday: "12-07-1980", // 7-Dec-1980
+    birthday: "1980-12-07", // 7-Dec-1980
     joinDate: "2019-10-19", //10-Oct-19
     
   },
   {
     id: 3,
     name: "Muthamma Pandu",
-    birthday: "02-06-1970", // 6-Feb-1970
+    birthday: "1970-02-06", // 6-Feb-1970
     joinDate: "2022-01-15", //1-Feb-20
 
   },
   {
     id: 4,
     name: "Venkatesh Arumugam",
-    birthday: "12-17-1999", // 17-Dec-1999
+    birthday: "1999-12-17", // 17-Dec-1999
     joinDate: "2020-11-05", //5-Nov-20
 
   },
   {
     id: 5,
     name: "Lokesh Venkatesan",
-    birthday: "05-10-1996", // 10-May-1996
+    birthday: "1996-05-10", // 10-May-1996
     joinDate: "2020-12-01", // 1-Dec-20
 
   },
   {
     id: 6,
     name: "Rathinavel Palaniyandi",
-    birthday: "07-07-1995", // 7-Jul-1995
+    birthday: "1995-07-07", // 7-Jul-1995
     joinDate:"2020-12-01", // 1-Dec-20
 
   },
   {
     id: 7,
     name: "S.Karthik",
-    birthday: "04-22-1998", // 22-Apr-1998
+    birthday: "1998-04-22", // 22-Apr-1998
     joinDate: "2021-04-02", //2-Apr-21
 
   },
   {
     id: 8,
     name: "S. Shunmugaraj",
-    birthday: "02-16-1992", // 16-Feb-1992
+    birthday: "1992-02-16", // 16-Feb-1992
     joinDate: "2021-08-23", //23-Aug-21
 
   },
   {
     id: 9,
     name: "",
-    birthday: "04-04-2002", // 4-Apr-2002
+    birthday: "2002-04-04", // 4-Apr-2002
     joinDate: "2021-09-20", //20-Sep-21
 
   },
   {
     id: 10,
     name: "P. Yuvarajasekar",
-    birthday: "02-01-2000", // 1-Feb-2000
+    birthday: "2000-02-01", // 1-Feb-2000
     joinDate: "2021-11-08", //8-Nov-21
 
   },
   {
     id: 11,
     name: "P. Sudhakar",
-    birthday: "01-09-1988", // 28-Oct-1988
+    birthday: "1988-01-09", // 28-Oct-1988
     joinDate: "2021-11-10", //10-Nov-21
 
   },
   {
     id: 12,
     name: "Ramkumar",
-    birthday: "02-24-1995", //24-Feb-1995
+    birthday: "1995-02-24", //24-Feb-1995
     joinDate: "2021-11-01",//1-Nov-21
 
   }
