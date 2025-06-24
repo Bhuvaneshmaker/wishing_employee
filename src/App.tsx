634476fe1d4fc5d4876fe3cb7bd0d3afd8cf9d8e
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-06-24 22:29:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,39 +16,39 @@ const EmployeeBirthdayApp = () => {
   // Sample data - replace with your Excel data
     const sampleEmployees = [
 {
-    id: 1,
-    name: "Hemnath Chandrasekaran",
-    birthday: "1994-09-02", // 2-Sep-1994
-    joinDate: "2017-05-17", //8-May-17
-
+    "id": 1,
+    "name": "Hemnath Chandrasekaran",
+    "birthday": "Friday, September 02, 1994",
+    "joinDate": "Monday, May 08, 2017",
+    "": ""
   },
   {
-    id: 2,
-    name: "K ShyamalaDevi",
-    birthday: "1980-12-07", // 7-Dec-1980
-    joinDate: "2019-10-19", //10-Oct-19
-    
+    "id": 2,
+    "name": "K ShyamalaDevi",
+    "birthday": "Sunday, December 07, 1980",
+    "joinDate": "Thursday, October 10, 2019",
+    "": ""
   },
   {
-    id: 3,
-    name: "Muthamma Pandu",
-    birthday: "1970-02-06", // 6-Feb-1970
-    joinDate: "2022-01-15", //1-Feb-20
-
+    "id": 3,
+    "name": "Muthamma Pandu",
+    "birthday": "Friday, February 06, 1970",
+    "joinDate": "Saturday, February 01, 2020",
+    "": ""
   },
   {
-    id: 4,
-    name: "Venkatesh Arumugam",
-    birthday: "1999-12-17", // 17-Dec-1999
-    joinDate: "2020-11-05", //5-Nov-20
-
+    "id": 4,
+    "name": "Venkatesh Arumugam",
+    "birthday": "Friday, December 17, 1999",
+    "joinDate": "Thursday, November 05, 2020",
+    "": ""
   },
   {
-    id: 5,
-    name: "Lokesh Venkatesan",
-    birthday: "1996-05-10", // 10-May-1996
-    joinDate: "2020-12-01", // 1-Dec-20
-
+    "id": 5,
+    "name": "Lokesh Venkatesan",
+    "birthday": "Friday, May 10, 1996",
+    "joinDate": "Tuesday, December 01, 2020",
+    "": ""
   },
   ];
   useEffect(() => {
